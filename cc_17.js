7571416863abccb05954f4abe4c1e7e7127ce955
Maidele Archelus <maidele@usf.edu>
2025-03-30 23:51:40 -0400

    Task 2: Create a SalesRep Class

--- cc_17.js
+++ cc_17.js
@@ -21,5 +21,33 @@ class Customer {
   const customer1 = new Customer('Jess Doe', 'jess@gmail.com');
   customer1.addPurchase(50);
   customer1.addPurchase(100);
-  console.log(`${customer1.name} total spent: $${customer1.getTotalSpent()}`); 
+  console.log(`${customer1.name} total spent: $${customer1.getTotalSpent()}`);
+  
+  // Task 2:
+class SalesRep {
+    constructor(name) {
+      this.name = name;
+      this.clients = [];
+    }//properties for salesrep
+
+     addClient(customer) {
+      this.clients.push(customer);
+    }//adds customer
+
+     getClientTotal(name) {
+      const client = this.clients.find(client => client.name === name);
+      return client ? client.getTotalSpent() : 50;
+    }
+  }//finds name of client
+
+   // Logging Test #2
+  const salesRep = new SalesRep('Jessica Will');
+  salesRep.addClient(customer1);
+  
+  console.log(`spent by Alice in total: $${salesRep.getClientTotal('Alice')}`);
+
+ 
+
+ 
+ 
 
